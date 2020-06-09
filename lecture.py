3a1cca2d781008ee3845a6de3359127e650b8725
Matt Gill <matt@dustcloudlabs.com>
2020-06-08 20:04:09 -0400

    lecture.py

---
+++ lecture.py
@@ -0,0 +1,28 @@
+# OOP
+## Objects or Class
+### blueprints/prototypes
+### holds data :attributes
+### methods:functions
+
+# Pet Blueprint (Class)
+## name, height, gender, species, diet, number of legs
+### feed_pet
+### play()
+
+# Pet("rover","2","M","dog",etc etc)
+
+class Store:
+    #attributes
+    #name
+    #categories
+    #inventory
+    #employees
+
+    # constructor
+    def __init__(self, name, categories):
+        self.name = name
+        self.categories = categories
+        self.employees = []
+    
+    def __
+        
